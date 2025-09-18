5891b4767b3656ee8269b0917124516c68e9e31f
suroju-hom-sai-santosh <surojuhomsaisantosh@gmail.com>
2025-09-18 22:37:37 +0530

    updated the file three again wanted to test new cmd

--- filethree.java
+++ filethree.java
@@ -2,5 +2,9 @@ public class filethree {
 
      public static void main(String[] args) {
         System.out.println( "This is file three");
+
+        System.out.println( "Testing new command ");
+
+        System.out.println("again checking the new command");
      }
 }
