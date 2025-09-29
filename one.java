0430295867b0775341833bd5c6236faae66aa34f
suroju-hom-sai-santosh <surojuhomsaisantosh@gmail.com>
2025-09-29 06:56:58 +0530

    Testing the am function

--- one.java
+++ one.java
@@ -3,6 +3,7 @@ public class one
 
     public static void main(String[] args) {
         System.out.println("This is file one with java extension");
+        System.out.println("Now i am about to test the git commit -am" +"your message" +"function");
     }
     
 }
